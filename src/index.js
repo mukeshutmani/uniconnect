24648dc57a0db8e3b223cc74ac785cca3012a0cb
mukeshutmani <kumarmukesh789123@gmail.com>
2025-01-15 14:40:05 +0500

    utils code changes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 mongoose.set("strictQuery", false)
-
-
-
-connectDB()
 
 
 dotenv.config({
     path: "./env"
 })
+
+connectDB()
+.then(() => {
+     app.on("error", (error) => {
+          console.log("ERR:", error);
+          throw error       
+     })
+     app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT: ${process.env.PORT}`);
+     })
+})
+.catch((error) => {
+   console.log("MongoDB connection fail:", error);
+})
+
+
 
 
 /*
